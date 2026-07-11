6929ca7334c416fedab8ca27b50cd6ed96346587
mohdayan0306-web <mohdayan0306@gmail.com>
2026-07-11 15:43:22 +0530

    Make understanding about Variables in java  and also learn about premeative datatypes

--- demo.java
+++ demo.java
@@ -1,5 +1,28 @@
-public class demo{
-    public static void main(String[] args){
-        System.out.println("hello java !");
+
+public class demo {
+
+    public static void main(String[] args) {
+        // Data types(premative)
+        //Integers --> byte ,short , int, long
+        byte num1 = 10;
+        short num2 = 20;
+        int num3 = 30;
+        long number = 6329772;
+        System.err.println(num1 + " ," + num2 + ", " + num3 + " ," + number);
+
+        //Real numeber --> float, Double 
+        float x = 2.34f;  //single precision
+        double y = 32.7586903;//double precision  which is mostly used 
+
+        System.err.println("flaot -> " + x + " double-> " + y);
+
+        //booleans 
+        boolean istrue = true;
+        System.out.println(istrue);
+
+        //char
+        char latter = 'A';
+        System.out.println(latter);
+
     }
 }
