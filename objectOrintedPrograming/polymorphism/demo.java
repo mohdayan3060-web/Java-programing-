8c50c0ccbb82be211d7fe3da88f21a59986285ec
mohdayan0306-web <mohdayan0306@gmail.com>
2026-07-21 19:34:16 +0530

    Learn about Polymorphism | How To implement their code |Also Understand their types

--- objectOrintedPrograming/polymorphism/demo.java
+++ objectOrintedPrograming/polymorphism/demo.java
@@ -1,5 +1,26 @@
+//Polymorphism --> Ablity of object behave diffrently 
+//polymorphism achive through methods overloading and methods overridding 
+//Polymorphism are two type Runtime(methods overridding ) and Compile(methods overloading ) time 
+
 public class demo{
     public static void main(String[] args) {
+        // demonstrating polymorphism at runtime
+        Person p= new Father();
+        p.role();
         
+    }
+}
+
+class Person{
+
+    void role(){
+        System.out.println("I am Person");
+    }
+}
+
+class Father extends Person{
+    @Override
+    void role (){
+        System.out.println("I am Father ");
     }
 }
